3158d32720f108054c5cec345ec9fc9d795f62ec
Michael Hierweck <michael@hierweck.de>
2014-06-22 12:52:53 +0200

    Dropped intersphinx and github support.

--- source/conf.py
+++ source/conf.py
@@ -27,10 +27,8 @@ sys.path.insert(0, os.path.abspath('_ext'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.graphviz',
-    'edit_on_github',
     ]
 
 # Add any paths that contain templates here, relative to this directory.
