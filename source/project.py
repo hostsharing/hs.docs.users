a6a7d8c98a723dc9e3e56a93b3086a4bef63c524
Michael Hierweck <michael@hierweck.de>
2015-03-01 12:07:48 +0100

    Fixed unicode problem.

--- source/project.py
+++ source/project.py
@@ -12,6 +12,6 @@ author = u'Hostsharing eG'
 
 published = date.today()
 copyright = u'2000-{year}, {author}'.format(year=published.year, author=author)
-today = published.strftime('%-d. %B %Y')
-release = published.strftime('%Y-%m-%d')
-version = published.strftime('%Y-%m-%d')
+today = published.strftime('%-d. %B %Y').decode('utf-8')
+release = published.strftime('%Y-%m-%d').decode('utf-8')
+version = published.strftime('%Y-%m-%d').decode('utf-8')
