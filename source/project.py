055884fa6c273a960da0ed85840928456084bb5a
Michael Hierweck <michael@hierweck.de>
2017-03-25 17:44:44 +0100

    Fixed inclusion order of project configuration.

---
+++ source/project.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# These project configuration settings override the defaults
+# provided by defaults.py.
+
+from datetime import date
+
+
+# General information about the project.
+
+code = 'hsusers'
+project = u'Benutzerhandbuch'
+author = u'Hostsharing eG'
+published = date.today()
+orientation = 'portrait'
+
+
+# Project specific customization.
